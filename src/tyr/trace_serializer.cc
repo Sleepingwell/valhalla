68e1c0bd648fa306966b25971968bc73d2521017
Simon Knapp <simon.knapp@csiro.au>
2021-08-12 11:23:47 +1000

    Add OSM id for start node of edge.

--- src/tyr/trace_serializer.cc
+++ src/tyr/trace_serializer.cc
@@ -140,6 +140,9 @@ json::ArrayPtr serialize_edges(const AttributesController& controller,
       if (edge.has_way_id()) {
         edge_map->emplace("way_id", static_cast<uint64_t>(edge.way_id()));
       }
+      if(trip_path.node(i - 1).has_osmid()) {
+        edge_map->emplace("osmid", static_cast<uint64_t>(trip_path.node(i - 1).osmid()));
+      }
       if (edge.has_id()) {
         edge_map->emplace("id", static_cast<uint64_t>(edge.id()));
       }
